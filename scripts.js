0da51d21a546c4fc8a4c7c307d016a1b45f690e8
Cody <codyphillips@ymail.com>
2024-06-22 12:54:06 -0500

    Finally solved getting boxes to grow and shrink accordingly. Also added error handling user input

--- scripts.js
+++ scripts.js
@@ -1,24 +1,40 @@
 "use strict";
 
 const generateGridButton = document.getElementById("generate-grid-button");
-let grid = gridGenerator(256);
+let grid = gridGenerator(16);
 const gridDiv = grid.querySelectorAll("div");
 
-function gridGenerator(totalGridPixels) {
-  const grid = document.querySelector(".grid-container");
+function gridGenerator(heightAndWidth) {
+  const container = document.querySelector(".grid-container");
+  const totalArea = heightAndWidth * heightAndWidth;
 
-  for (let i = 0; i < totalGridPixels; i++) {
+  for (let i = 0; i < totalArea; i++) {
     const newDiv = document.createElement("div");
-    grid.appendChild(newDiv);
+    newDiv.id = "drawing-div";
+    newDiv.style.width = `calc(100% / ${heightAndWidth})`;
+    newDiv.style.height = `calc(100% / ${heightAndWidth})`;
+    container.appendChild(newDiv);
     newDiv.addEventListener("mouseover", () => {
-      newDiv.id = "draw";
+      newDiv.style.backgroundColor = "white";
     });
   }
-  return grid;
+
+  return container;
 }
 
+function createGridErrorHandling(userInput) {
+  if (Number(userInput) && userInput <= 100) {
+    grid.textContent = "";
+    grid = gridGenerator(Number(userInput));
+  } else if (userInput > 100) {
+    alert(
+      "100 and above will affect performance, please choose a lower number"
+    );
+  } else {
+    alert("Invalid Input");
+  }
+}
 generateGridButton.addEventListener("click", () => {
-  grid.textContent = "";
-  const userInput = prompt("Enter grid resolution:");
-  grid = gridGenerator(userInput);
+  const userInput = prompt("Enter grid pixels on each side");
+  createGridErrorHandling(userInput);
 });
