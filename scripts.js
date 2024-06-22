b411b15a3c7a1bc5a8d7c7c1e34c8cdbd8ff2556
Cody <codyphillips@ymail.com>
2024-06-22 11:26:48 -0500

    condensed some code and made it more readable

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 "use strict";
 
-const createButton = document.getElementById("create-grid-button");
-const grid = gridGenerator();
+const generateGridButton = document.getElementById("generate-grid-button");
+let grid = gridGenerator(256);
 const gridDiv = grid.querySelectorAll("div");
 
-gridDiv.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.id = "draw";
-  });
-});
-
-function gridGenerator() {
+function gridGenerator(totalGridPixels) {
   const grid = document.querySelector(".grid-container");
 
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < totalGridPixels; i++) {
     const newDiv = document.createElement("div");
     grid.appendChild(newDiv);
+    newDiv.addEventListener("mouseover", () => {
+      newDiv.id = "draw";
+    });
   }
   return grid;
 }
+
+generateGridButton.addEventListener("click", () => {
+  grid.textContent = "";
+  const userInput = prompt("Enter grid resolution:");
+  grid = gridGenerator(userInput);
+});
