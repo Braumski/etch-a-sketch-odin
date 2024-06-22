7f4c0fbaff8b4f99da4935bdb6a25dc8a1b5ec22
Cody <codyphillips@ymail.com>
2024-06-22 10:45:58 -0500

    Renamed some variables for better clarity.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const container = document.querySelector(".grid-container");
 const createButton = document.getElementById("create-grid-button");
 const grid = gridGenerator();
-
-function gridGenerator() {
-  for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv);
-  }
-  return container;
-}
-
-const gridDiv = container.querySelectorAll("div");
+const gridDiv = grid.querySelectorAll("div");
 
 gridDiv.forEach((div) => {
   div.addEventListener("mouseover", () => {
     div.id = "draw";
   });
 });
+
+function gridGenerator() {
+  const grid = document.querySelector(".grid-container");
+
+  for (let i = 0; i < 256; i++) {
+    const newDiv = document.createElement("div");
+    grid.appendChild(newDiv);
+  }
+  return grid;
+}
